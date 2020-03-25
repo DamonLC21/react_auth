30ae40aa58da814d8709bf9e38a6c127731681b5
Damon Chivers <chiversdamon@gmail.com>
2020-03-25 12:39:56 -0600

    add backend url variables

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import Login from './components/Login';
+const loginUrl = "http://localhost:3000/api/v1/login"
+const profileUrl = "http://localhost:3000/api/v1/profile"
 
 class App extends Component {
   render(){
